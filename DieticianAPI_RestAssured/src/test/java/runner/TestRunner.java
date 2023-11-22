564ef2863e192489685b821e0522f4afb073ca3b
Adeline Grace <adelinegrace.a@gmail.com>
2023-11-21 21:47:22 -0500

    check

--- DieticianAPI_RestAssured/src/test/java/runner/TestRunner.java
+++ DieticianAPI_RestAssured/src/test/java/runner/TestRunner.java
@@ -12,9 +12,8 @@ import io.cucumber.testng.CucumberOptions;
 					}, 
 			monochrome=false,  
 
-			features = {"src/test/resources/Feature/BMorbidity.feature",
-					
-					"src/test/resources/Feature/GetDeleteValidationS.feature"}, 
+			features = {"src/test/resources/Feature"	
+					}, 
 
 			glue= {"api.stepdefinition"} 
 		)
